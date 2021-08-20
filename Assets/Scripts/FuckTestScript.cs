8629c36276fdbd29694cf2332d07c8e2f64421ed
猫刀刀 <kunkun0307@qq.com>
2021-08-20 17:02:10 +0800

    Update FuckTestScript.cs

--- Assets/Scripts/FuckTestScript.cs
+++ Assets/Scripts/FuckTestScript.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using OdinSerializer;
+using System.IO;
 
-public class FuckTestScript : MonoBehaviour
+public static class Example
 {
-   
-    
+	public static void Save(MyData data, string filePath)
+	{
+		byte[] bytes = SerializationUtility.SerializeValue(data, DataFormat.Binary);
+		File.WriteAllBytes(filePath, bytes);
+	}
+
+	public static MyData Load(string filePath)
+	{
+		byte[] bytes = File.ReadAllBytes(filePath);
+		return SerializationUtility.DeserializeValue<MyData>(bytes, DataFormat.Binary);
+	}
+}
+
+public class MyData
+{
+	
 }
