95f499a8196dbc4151489f41fe814f42c89b4ec1
猫刀刀 <kunkun0307@qq.com>
2021-08-21 20:36:51 +0800

    模型导入  寻路添加
    模型导入  寻路添加

---
+++ Assets/Scripts/PathFindingTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pathfinding
+{
+    public class PathFindingTest : VersionedMonoBehaviour
+    {
+
+        public Transform target;
+        IAstarAI ai;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+
+        void OnEnable()
+        {
+            ai = GetComponent<IAstarAI>();
+            // Update the destination right before searching for a path as well.
+            // This is enough in theory, but this script will also update the destination every
+            // frame as the destination is used for debugging and may be used for other things by other
+            // scripts as well. So it makes sense that it is up to date every frame.
+            if (ai != null) ai.onSearchPath += Update;
+        }
+
+        void OnDisable()
+        {
+            if (ai != null) ai.onSearchPath -= Update;
+        }
+
+        /// <summary>Updates the AI's destination every frame</summary>
+        void Update()
+        {
+            if (target != null && ai != null)
+            {
+                if (ai.destination == target.position)
+                    return;
+
+                ai.destination = target.position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateTarget.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace Pathfinding
+{
+    public class UpdateTarget : MonoBehaviour
+    {
+        public Transform target;
+
+        public LayerMask mask;
+        IAstarAI[] ais;
+        Camera main;
+
+        private void Awake()
+        {
+            main = GetComponent<Camera>();
+            ais = FindObjectsOfType<MonoBehaviour>().OfType<IAstarAI>().ToArray();
+        }
+
+        bool isClick = false;
+
+        public void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Debug.LogError("点击点击点击点击点击点击");
+                isClick = true;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.LogError("点击点击点击点击点击点击");
+                isClick = true;
+            }
+        }
+
+        public void LateUpdate()
+        {
+            if (isClick)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, mask))
+                {
+                    target.position = hit.point;
+
+                    for (int i = 0; i < ais.Length; i++)
+                    {
+                        ais[i].SearchPath();
+                    }
+
+                    //positionFound = true;
+                }
+                isClick = false;
+            }
+
+           
+        }
+
+
+    }
+
+}
+
