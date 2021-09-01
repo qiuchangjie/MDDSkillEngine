6aa08313b77a292bca299017303837ad17fe6a42
猫刀刀 <kunkun0307@qq.com>
2021-09-01 23:29:46 +0800

    Update CatAssetManager.cs

--- Assets/CatAsset/Runtime/Core/CatAssetManager.cs
+++ Assets/CatAsset/Runtime/Core/CatAssetManager.cs
@@ -8,6 +8,15 @@ using UnityEngine.SceneManagement;
 namespace CatAsset
 {
     /// <summary>
+    /// 加载资源成功回调函数。
+    /// </summary>
+    /// <param name="assetName">要加载的资源名称。</param>
+    /// <param name="asset">已加载的资源。</param>
+    /// <param name="duration">加载持续时间。</param>
+    /// <param name="userData">用户自定义数据。</param>
+    public delegate void LoadAssetSuccessCallback(string assetName, object asset, float duration, object userData);
+
+    /// <summary>
     /// CatAsset管理器
     /// </summary>
     public class CatAssetManager
