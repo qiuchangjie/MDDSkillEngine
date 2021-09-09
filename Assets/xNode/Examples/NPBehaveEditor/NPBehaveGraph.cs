c139201f6a92dc5caea1a6a6a33a871200698b71
猫刀刀 <kunkun0307@qq.com>
2021-09-09 23:42:51 +0800

    行为树可视化初试
    行为树可视化初试

---
+++ Assets/xNode/Examples/NPBehaveEditor/NPBehaveGraph.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateAssetMenu(fileName = "NewNPBehaveGraph", menuName = "NewNPBehaveGraph/test")]
+public class NPBehaveGraph : NodeGraph { 
+	
+}
