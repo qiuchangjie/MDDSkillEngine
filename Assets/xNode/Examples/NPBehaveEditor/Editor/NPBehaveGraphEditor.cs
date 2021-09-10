c095e6f170cffa0feb464cab4964a42580a2a109
猫刀刀 <kunkun0307@qq.com>
2021-09-10 17:15:47 +0800

    行为树节点编辑0.002
    行为树节点编辑0.002

---
+++ Assets/xNode/Examples/NPBehaveEditor/Editor/NPBehaveGraphEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode.Examples;
+using XNode;
+using XNodeEditor;
+
+namespace NPBehave.node
+{
+	[CustomNodeGraphEditor(typeof(MathGraphEditor))]
+	public class MathGraphEditor : NodeGraphEditor
+	{
+
+		/// <summary> 
+		/// Overriding GetNodeMenuName lets you control if and how nodes are categorized.
+		/// In this example we are sorting out all node types that are not in the XNode.Examples namespace.
+		/// </summary>
+		public override string GetNodeMenuName(System.Type type)
+		{
+			if (type.Namespace == "NPBehave.node")
+			{
+				return base.GetNodeMenuName(type).Replace("NPBehave/node", "");
+			}
+			else return null;
+		}
+	}
+}
---
+++ Assets/xNode/Examples/NPBehaveEditor/Nodes/RootNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using NPBehave;
+
+namespace NPBehave.node
+{
+	public class RootNode : XNode.Node
+	{
+
+		[Output] 
+		public int Root;
+
+
+		// Use this for initialization
+		protected override void Init()
+		{
+			base.Init();
+
+		}
+
+		// Return the correct value of an output port when requested
+		public override object GetValue(NodePort port)
+		{
+			return null; // Replace this
+		}
+	}
+}
+
