c2c00da51845b02e24c8faab4478e7d5619581ac
猫刀刀 <kunkun0307@qq.com>
2022-02-12 17:55:50 +0800

    技能数据timeline导出

---
+++ Assets/MDDSkillEngine/Scripts/Runtime/Skill/SkillDataModel/SkillDataBase.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MDDSkillEngine
+{
+    /// <summary>
+    /// 释后技能数据模板基类
+    /// </summary>
+    public abstract class SkillDataBase
+    {
+        private SkillDataType m_DataType;
+
+        private string m_ResouceName = "None";
+
+        private float m_StartTime;
+
+        private float m_EndTime;
+
+        public SkillDataType DataType
+        {
+            get { return m_DataType; }
+            set { m_DataType = value; }
+        }
+
+        public string ResouceName
+        {
+            get { return m_ResouceName; }
+            set { m_ResouceName = value; }
+        }
+
+        public float StartTime
+        {
+            get { return m_StartTime; }
+            set { m_StartTime = value; }
+        }
+
+        public float EndTime
+        {
+            get { return m_EndTime; }
+            set { m_EndTime = value; }
+        }
+    }
+
+    public class SkillData
+    {
+        public List<SkillDataBase> skillData=new List<SkillDataBase>();
+        
+    }
+
+
+    /// <summary>
+    /// 技能数据的类型
+    /// 数据的源头来自于timeline编辑的导出数据
+    /// </summary>
+    public enum SkillDataType
+    {
+        None,
+        Animation,
+        Effect,
+        Collider,
+        Move,
+        Camera,
+        Sound,
+    }
+
+}
+
